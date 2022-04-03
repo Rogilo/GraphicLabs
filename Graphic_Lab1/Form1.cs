d495e6d33de563a8a03583c6b7bc95c97819d239
Volodymyr Matiishchuk <Volodymyr.matiishchuk@gmail.com>
2022-04-03 15:53:47 +0300

    Bug fix

--- Graphic_Lab1/Form1.cs
+++ Graphic_Lab1/Form1.cs
@@ -5,9 +5,7 @@ namespace Graphic_Lab1
         private bool Desenhar = false;
         private bool Matric = false;
         private List<Point> points = new List<Point>();
-        private List<Point> pointsMatrix = new List<Point>();
         Point[] points1;
-        Point[] points2;
         public Graphic_Lab1()
         {
             InitializeComponent();
@@ -56,37 +54,29 @@ namespace Graphic_Lab1
             }
             if (Matric == true)
             {
+                int x1, x2, x3, y1, y2, y3;
                 int p1 = int.Parse(textBox3.Text);
                 int p2 = int.Parse(textBox4.Text);
                 int p3 = int.Parse(textBox5.Text);
                 int p4 = int.Parse(textBox6.Text);
                 if (points1.Length == 1)
                 {
-                    int x1, y1;
                     x1 = p1 * (points1[0].X - xOrigem) + p3 * (yOrigem - points1[0].Y);
                     y1 = p2 * (points1[0].X - xOrigem) + p4 * (yOrigem - points1[0].Y);
-                    Point point2 = new Point(xOrigem + x1, yOrigem - y1);
-                    pointsMatrix.Add(point2);
-                    points2 = pointsMatrix.Distinct().ToArray();
                     g.FillRectangle(new SolidBrush(Color.Red), x1 + xOrigem, yOrigem - y1, 3, 3);
                 }
                 else if(points1.Length == 2) 
                 {
-                    int x1,x2, y1,y2;
                     x1 = p1 * (points1[0].X - xOrigem) + p3 * (yOrigem - points1[0].Y);
                     y1 = p2 * (points1[0].X - xOrigem) + p4 * (yOrigem - points1[0].Y);
                     x2 = p1 * (points1[1].X - xOrigem) + p3 * (yOrigem - points1[1].Y);
                     y2 = p2 * (points1[1].X - xOrigem) + p4 * (yOrigem - points1[1].Y);
                     Point point3 = new Point(xOrigem + x1, yOrigem - y1);
                     Point point4 = new Point(xOrigem + x2, yOrigem - y2);
-                    pointsMatrix.Add(point3);
-                    pointsMatrix.Add(point4);
-                    points2 = pointsMatrix.Distinct().ToArray();
-                    g.DrawLine(Pens.Red, points2[0], points2[1]);
+                    g.DrawLine(Pens.Red, point3, point4);
                 }
                 else
                 {
-                    int x1, x2,x3, y1, y2,y3;
                     x1 = p1 * (points1[0].X - xOrigem) + p3 * (yOrigem - points1[0].Y);
                     y1 = p2 * (points1[0].X - xOrigem) + p4 * (yOrigem - points1[0].Y);
                     x2 = p1 * (points1[1].X - xOrigem) + p3 * (yOrigem - points1[1].Y);
@@ -96,13 +86,9 @@ namespace Graphic_Lab1
                     Point point5 = new Point(xOrigem + x1, yOrigem - y1);
                     Point point6 = new Point(xOrigem + x2, yOrigem - y2);
                     Point point7 = new Point(xOrigem + x3, yOrigem - y3);
-                    pointsMatrix.Add(point5);
-                    pointsMatrix.Add(point6);
-                    pointsMatrix.Add(point7);
-                    points2 = pointsMatrix.Distinct().ToArray();
-                    g.DrawLine(Pens.Red, points2[0], points2[1]);
-                    g.DrawLine(Pens.Red, points2[1], points2[2]);
-                    g.DrawLine(Pens.Red, points2[0], points2[2]);
+                    g.DrawLine(Pens.Red, point5, point6);
+                    g.DrawLine(Pens.Red, point6, point7);
+                    g.DrawLine(Pens.Red, point5, point7);
                 }
             }
         }
@@ -142,14 +128,6 @@ namespace Graphic_Lab1
             Matric = false;
         }
         private void TextBox6_TextChanged(object sender, EventArgs e)
-        {
-            Matric = false;
-        }
-        private void TextBox7_TextChanged(object sender, EventArgs e)
-        {
-            Matric = false;
-        }
-        private void TextBox8_TextChanged(object sender, EventArgs e)
         {
             Matric = false;
         }
